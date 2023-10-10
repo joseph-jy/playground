a073e0a90c6d3ddb4f490d9a490e7f0f93aa48cf
joseph.jyoon <joseph.jyoon@kakaocorp.com>
2023-10-10 18:56:35 +0900

    minor changes

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Flask!'
+@app.route('/api/v1/hello', methods=['GET'])
+def api_hello():
+    return jsonify(message="Hello, World!")
 
 if __name__ == '__main__':
     app.run()
