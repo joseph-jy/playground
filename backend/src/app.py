719d1f9b923b92e701ae3a654166cd5414c49c96
Joseph Yoon <sidnancy81@gmail.com>
2023-12-10 00:25:54 +0900

    ## changes  - made backend framwork python flask

--- backend/src/app.py
+++ backend/src/app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/api/data', methods=['GET'])
-def get_data():
+def get_data(): 
     data = {
         'message': 'Hello World!',
         'status': 200
